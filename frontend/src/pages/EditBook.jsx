5345d2938bc813b22fbb26a29f69682f32a38efe
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-08-13 22:40:20 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ export default function EditBook() {
     useEffect(()=>{
         setLoading(true)
         axios
-        .get(`http://localhost:5555/books/${id}`)
+        .get(`https://book-store-git-main-jishans-projects-80682501.vercel.app/books/${id}`)
         .then((res) => {
             setAuthor(res.data.author)
             setTitle(res.data.title)
