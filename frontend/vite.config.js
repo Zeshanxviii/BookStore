80cbbd92d81540b38b7388360430e45c8d0d0208
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-08-14 20:58:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    Proxy:{
-      '/api': 'http://localhost:5555'
-    }
-  },
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy all API requests to the backend server
+      '/api': {
+        target: 'https://book-store-i9cu1vkp8-jishans-projects-80682501.vercel.app/"', // Replace with your backend server URL
+        changeOrigin: true,
+        secure: false // Optional: If your backend doesn't have the `/api` prefix
+      },
+    },
+  },
+});
