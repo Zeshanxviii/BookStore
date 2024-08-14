2c1d0698cee2c000161fc53e331adc6c6fa5add9
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-08-14 20:45:09 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import axios from 'axios'
 
-axios.default.BaseURL = "https://book-store-i9cu1vkp8-jishans-projects-80682501.vercel.app/"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
