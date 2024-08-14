6861bd299675e8dbe32565a403a618b456569953
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-08-14 21:17:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Proxy all API requests to the backend server
       '/api': {
-        target: 'https://book-store-lac-sigma.vercel.app/"', // Replace with your backend server URL
+        target: 'https://book-store-i9cu1vkp8-jishans-projects-80682501.vercel.app/', // Replace with your backend server URL
         changeOrigin: true,
         secure: false // Optional: If your backend doesn't have the `/api` prefix
       },
