c05b83196f13f2eb3bcb76a519a08076607ea2be
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-08-17 20:46:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://book-store-lac-sigma.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 });
